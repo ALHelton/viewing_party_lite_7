6b453ce5106859e9c8d4a923c7b395a20192a1a7
Andra Helton <andrahelton@gmail.com>
2023-04-03 14:47:51 -0600

    Add model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  let!(:andra) { User.create!(name: "Andra", email: "andra@turing.edu") }
-  let!(:hady) { User.create!(name: "Hady", email: "hady@turing.edu") }
+  let!(:andra) { User.create!(name: "Andra", email: "andra@turing.edu", password: "hiya123") }
+  let!(:hady) { User.create!(name: "Hady", email: "hady@turing.edu", password: "heyo888") }
 
   let!(:halloween) { Party.create!(name: "Halloween Party", user_id: andra.id, party_date: "2023/10/31", party_time: "10:30", duration: 123) }
   let!(:girls_night) { Party.create!(name: "Girl's Night", user_id: hady.id, party_date: "2023/10/31", party_time: "02:00", duration: 210) }
@@ -14,10 +14,25 @@ RSpec.describe User, type: :model do
   let!(:usp3) { UserParty.create!(user_id: andra.id, party_id: eighties.id, invite_status: 1) }
   let!(:usp4) { UserParty.create!(user_id: hady.id, party_id: eighties.id, invite_status: 2) }
 
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+    it { should have_secure_password }
+  end
 
   describe "relationships" do
     it { should have_many :user_parties }
     it { should have_many(:parties).through(:user_parties) }
+  end
+
+  describe "attributes" do
+    it "should have correct attributes" do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
   end
 
   describe "instance methods" do
