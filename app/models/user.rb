3f3ed51f3f0083222dba0527964bab04e28bfda7
Andra Helton <andrahelton@gmail.com>
2023-04-03 14:48:52 -0600

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
 
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password
+
+  has_secure_password
+
   def get_host_parties
     Party.where(user_id: self.id)
   end
