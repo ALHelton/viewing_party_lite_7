6ea86ae065f5704738e14e02dc48e97686c30e08
Andra Helton <andrahelton@gmail.com>
2023-04-04 15:07:14 -0600

    Add US2

--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'landing page, index', type: :feature do
-  describe "as a user" do
+  describe "as a Registered user" do
     before :each do
       @user1 = User.create!(name: "Andra", email: "andra@turing.edu", password: "heyo888")
       @user2 = User.create!(name: "Hady", email: "hady@turing.edu", password: "heyo888")
@@ -24,12 +24,30 @@ RSpec.describe 'landing page, index', type: :feature do
       #   expect(page).to have_link("#{@user1.name}'s Dashboard")
       # end 
 
+      it "The list of existing users is just a list of email addresses" do
+        user = User.create(name: "Andra", email: "funbucket13@gmail.com", password: "test")
+        click_button("Log In")
+        fill_in :email, with: "funbucket13@gmail.com"
+        fill_in :password, with: "test"
+        click_on "Log In"
+        expect(current_path).to eq("/users/#{user.id}")
+        click_link "Back to Landing Page"
+
+        within "#existing_users" do
+          expect(page).to have_content(@user1.email)
+          expect(page).to have_content(@user2.email)
+          expect(page).to have_content(user.email)
+        end
+      end
+
       # it "if you click on a user's dashboard link, you will be taken to that user's dashboard" do
       #   within("#existing_users") do 
       #     click_link("#{@user1.name}'s Dashboard")
       #     expect(current_path).to eq("/users/#{@user1.id}")
       #   end
       # end
+
+
 
       it "link to go back to landing page" do 
         expect(page).to have_link("Back to Landing Page")
@@ -73,14 +91,17 @@ RSpec.describe 'landing page, index', type: :feature do
   end 
 
   describe "As a default visitor" do
-    it "I do not see the section of the page that lists existing users" do
-      user = User.create(name: "Andra", email: "andra@gmail.com", password: "test")
-      user2 = User.create(name: "Hady", email: "hady@gmail.com", password: "test")
+    before :each do
+      @user1 = User.create!(name: "Andra", email: "andra@turing.edu", password: "heyo888")
+      @user2 = User.create!(name: "Hady", email: "hady@turing.edu", password: "heyo888")
+      visit "/"
+    end
 
+    it "I do not see the section of the page that lists existing users" do
       within "#existing_users" do
         expect(page).to_not have_content("Existing Users and Their Dashboards")
-        expect(page).to_not have_content(user.name)
-        expect(page).to_not have_content(user2.name)
+        expect(page).to_not have_content(@user1.name)
+        expect(page).to_not have_content(@user2.name)
       end
     end
   end
